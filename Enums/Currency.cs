2b2b96158562c334639154aeeba30b5eefc8d95b
Zeronka <yan.aliyev.2000@bk.ru>
2026-04-07 02:17:17 +0300

    Add Services and Enums layer

---
+++ Enums/Currency.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingSystem.Enums
+{
+    public enum Currency
+    {
+        RUB,
+        USD,
+        EUR,
+        KZT,
+        CNY
+    }
+}
